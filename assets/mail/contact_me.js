3e52fee4dfb1734e8d0a8a9b9663ea45a8b84dd4
Darlington Maposa <maposad04@gmail.com>
2023-08-03 15:38:32 +0200

    edited contactme.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://lzucw526b0.execute-api.eu-west-1.amazonaws.com/dev",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
